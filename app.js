a89326d7176ea5c366726dc882fb5b6ff6f11094
GwenRaq <gwen.raq@gmail.com>
2017-03-13 08:22:48 -1000

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Sanity Check");
+
+//Finding HTML Elements
+console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
