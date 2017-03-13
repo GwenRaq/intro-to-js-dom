f0f8d3af1c3758571220e664d298b3de66c59981
GwenRaq <gwen.raq@gmail.com>
2017-03-13 08:45:17 -1000

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 console.log("Sanity Check");
 
 //Finding HTML Elements
-console.log(document.getElementById("use-id-method"));
-var idElement = document.getElementById("use-id-method");
-console.log(idElement);
+console.log(document.getElementsById("use-id-method"));
+var idElements = document.getElementsById("use-id-method");
+console.log(idElements);
+
+// console.log(document.getElementByTagName("div"));
+var tagElements = document.getElementsByTagName("div");
+console.log(tagElements);
+
+// console.log(document.getElementByClassName("use-class-method"));
+var classElements = document.getElementsByClassName("use-class-method");
+console.log(classElements);
+
+//Changing HTML Elements
+console.log(idElement.innerHTML);
+idElement.innerHTML = "I have been changed";
+
+console.log(classElements[1]);
+classElements[1].innerHTML = "I have been changed by my class Name"
